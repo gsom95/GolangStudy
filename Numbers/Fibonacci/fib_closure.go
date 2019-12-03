2964278cd90a68de58defc0d46e0b94d95a6897c
Igor Sapozhnikov <igorsapozhnikov9@gmail.com>
2019-12-03 17:52:43 +0300

    Update fib_closure.go
    Remove main function.

--- Numbers/Fibonacci/fib_closure.go
+++ Numbers/Fibonacci/fib_closure.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // fibonacci is a function that returns a function that returns an int.
 // solution for https://tour.golang.org/moretypes/26
 func fibonacci() func() int {
@@ -14,12 +12,5 @@ func fibonacci() func() int {
 			current, next = next, current+next
 		}
 		return current
-	}
-}
-
-func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
 	}
 }
